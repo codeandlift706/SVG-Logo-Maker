0f73812cc570165c7815a8493f1b9c275ba26b9b
Code Queen <channguyen97@gmail.com>
2023-09-19 12:47:57 -0700

    created files and started pseudocode

---
+++ index.js
@@ -0,0 +1,44 @@
+//insert imports
+
+//imports to jest and inquirer
+
+// Runs the application using imports from lib/
+//import shapes.js
+
+
+
+
+
+/*
+pseudocode:
+
+
+variables
+
+
+functions
+
+When I open up the command line and run node index.js
+I answer a series of questions on what my logo design is like
+my series of questions;
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+
+
+the command line captures my user input and based on my user input: 
+generates a custom design
+console.log("Generated logo.svg")
+createFile named 'logo.svg', with the contents based on my user input
+
+
+
+processes
+
+
+*/
