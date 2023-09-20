7ad950471c9ea18ba32944d36f9b7b30f1aa7fe5
Code Queen <channguyen97@gmail.com>
2023-09-19 21:03:08 -0700

    added render functions

--- index.js
+++ index.js
@@ -2,10 +2,22 @@
 //imports jest and inquirer
 const fs = require('fs');
 const inquirer = require('inquirer');
-const shapes = require('./lib/shapes');
+const generateShapes = require('./lib/generateShapes');
 
 
 // TODO: Create an array of questions for user input
+/*When I open up the command line and run node index.js
+I answer a series of questions on what my logo design is like
+my series of questions = [
+WHEN I am prompted for text
+THEN I can enter up >= three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number #22ff00)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)]
+*/
 const questions = [
     {
         type: 'input',
@@ -37,41 +49,34 @@ const questions = [
 
 
 /*
-pseudocode:
-
-
-//variables
-
-
-//functions
-
-When I open up the command line and run node index.js
-I answer a series of questions on what my logo design is like
-my series of questions = [
-WHEN I am prompted for text
-THEN I can enter up >= three characters
-WHEN I am prompted for the text color
-THEN I can enter a color keyword (OR a hexadecimal number #22ff00)
-WHEN I am prompted for a shape
-THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-WHEN I am prompted for the shape's color
-THEN I can enter a color keyword (OR a hexadecimal number)
-]
-
-
-
 the command line captures my user input and based on my user input: 
 generates a custom design
 console.log("Generated logo.svg")
 createFile named 'logo.svg', with the contents based on my user input
 handler function
 writeFile
-
-
-init function
-
-
-//processes
-
-
 */
+
+// TODO: Create a function to write SVG file
+const writeToFile = (fileName, data) => {
+    fs.writeFile('logo.svg', generateShapes(data), (err) => {
+        err ? console.error(err) : console.log("Generated on file")
+    }
+    )
+}
+
+
+// TODO: Create a function to initialize app
+const init = () => {
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        console.log("Generated logo.svg")
+        writeToFile('logo.svg', data)
+    })
+}
+
+
+
+
+
