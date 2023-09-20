9e21060205c7bf33c458306e504511c08119377a
Code Queen <channguyen97@gmail.com>
2023-09-19 18:51:37 -0700

    installed inquirer and jest

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 //insert imports
-
-//imports to jest and inquirer
+//imports jest and inquirer
 
 // Runs the application using imports from lib/
 //import shapes.js
-
 
 
 
@@ -13,32 +11,38 @@
 pseudocode:
 
 
-variables
+//variables
 
 
-functions
+//functions
 
 When I open up the command line and run node index.js
 I answer a series of questions on what my logo design is like
-my series of questions;
+my series of questions = [
 WHEN I am prompted for text
-THEN I can enter up to three characters
+THEN I can enter up <= three characters
 WHEN I am prompted for the text color
-THEN I can enter a color keyword (OR a hexadecimal number)
+THEN I can enter a color keyword (OR a hexadecimal number #22ff00)
 WHEN I am prompted for a shape
 THEN I am presented with a list of shapes to choose from: circle, triangle, and square
 WHEN I am prompted for the shape's color
 THEN I can enter a color keyword (OR a hexadecimal number)
+]
+
 
 
 the command line captures my user input and based on my user input: 
 generates a custom design
 console.log("Generated logo.svg")
 createFile named 'logo.svg', with the contents based on my user input
+handler function
+writeFile
 
 
+init function
 
-processes
+
+//processes
 
 
 */
